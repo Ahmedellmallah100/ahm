26ef085f4bf4d90b3d1abcf8320d30aeccdc5a6c
Ahmed Elmallah <ah2005510@gmail.com>
2026-09-13 01:05:55 +0300

    Update Instruction_memory.v

--- src/Instruction_memory.v
+++ src/Instruction_memory.v
@@ -19,8 +19,32 @@ always @(*) begin
 end
 
 // Initialize memory from file
-initial begin
-    $readmemh("program.txt", mem);
+always @(*) begin
+    case (A[6:2])   // 5 بت عنوان = 32 موقع (word-aligned)
+        5'd0:  RD = 32'h00004033;
+        5'd1:  RD = 32'h00000093;
+        5'd2:  RD = 32'h00100113;
+        5'd3:  RD = 32'h00100193;
+        5'd4:  RD = 32'h00100213;
+        5'd5:  RD = 32'h00000293;
+        5'd6:  RD = 32'h00a00313;
+        5'd7:  RD = 32'h00000393;
+        5'd8:  RD = 32'h00418c63;
+        5'd9:  RD = 32'h00110133;
+        5'd10: RD = 32'h404181b3;
+        5'd11: RD = 32'h00229393;
+        5'd12: RD = 32'h0023a023;
+        5'd13: RD = 32'h00420a63;
+        5'd14: RD = 32'h002080b3;
+        5'd15: RD = 32'h004181b3;
+        5'd16: RD = 32'h00229393;
+        5'd17: RD = 32'h0013a023;
+        5'd18: RD = 32'h00128293;
+        5'd19: RD = 32'hfc62cae3;
+        5'd20: RD = 32'h00000000;
+        default: RD = 32'h00000000;
+    endcase
 end
+
 
 endmodule  // Instruction_memory
