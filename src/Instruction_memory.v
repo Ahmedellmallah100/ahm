dd2d3fc8be577ee5623bffa4768b73de86ebf8ea
Ahmed Elmallah <ah2005510@gmail.com>
2026-09-13 01:41:00 +0300

    Update Instruction_memory.v

--- src/Instruction_memory.v
+++ src/Instruction_memory.v
@@ -2,25 +2,14 @@ module Instruction_memory (
     A, RD
 );
 
-// Memory parameters
 parameter mem_width = 32;
 parameter mem_depth = 32;
 
-// Port declaration
 input  [31:0] A;
 output reg [31:0] RD;
 
-// Memory array
-reg [mem_width-1:0] mem [mem_depth-1:0];
-
-// Read operation (Asynchronous)
 always @(*) begin
-    RD = mem[A[6:2]];     // Word aligned
-end
-
-// Initialize memory from file
-always @(*) begin
-    case (A[6:2])   // 5 بت عنوان = 32 موقع (word-aligned)
+    case (A[6:2])
         5'd0:  RD = 32'h00004033;
         5'd1:  RD = 32'h00000093;
         5'd2:  RD = 32'h00100113;
@@ -46,5 +35,4 @@ always @(*) begin
     endcase
 end
 
-
-endmodule  // Instruction_memory
+endmodule
