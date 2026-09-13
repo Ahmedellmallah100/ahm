ab092a8a8e777cf4fecdbc04b3ae7283da222786
Ahmed Elmallah <ah2005510@gmail.com>
2026-09-14 00:24:53 +0300

    Update Instruction_memory.v

--- src/Instruction_memory.v
+++ src/Instruction_memory.v
@@ -1,36 +1,23 @@
 module Instruction_memory (
-    A, RD
+    input  [31:0] A,
+    output reg [31:0] RD
 );
-
-parameter mem_width = 32;
-parameter mem_depth = 32;
-
-input  [31:0] A;
-output reg [31:0] RD;
 
 always @(*) begin
     case (A[6:2])
-        5'd0:  RD = 32'h00004033;
-        5'd1:  RD = 32'h00000093;
-        5'd2:  RD = 32'h00100113;
-        5'd3:  RD = 32'h00100193;
-        5'd4:  RD = 32'h00100213;
-        5'd5:  RD = 32'h00000293;
-        5'd6:  RD = 32'h00a00313;
-        5'd7:  RD = 32'h00000393;
-        5'd8:  RD = 32'h00418c63;
-        5'd9:  RD = 32'h00110133;
-        5'd10: RD = 32'h404181b3;
-        5'd11: RD = 32'h00229393;
-        5'd12: RD = 32'h0023a023;
-        5'd13: RD = 32'h00420a63;
-        5'd14: RD = 32'h002080b3;
-        5'd15: RD = 32'h004181b3;
-        5'd16: RD = 32'h00229393;
-        5'd17: RD = 32'h0013a023;
-        5'd18: RD = 32'h00128293;
-        5'd19: RD = 32'hfc62cae3;
-        5'd20: RD = 32'h00000000;
+        5'd0:  RD = 32'h00500093; // ADDI x1, x0, 5
+        5'd1:  RD = 32'h00A00113; // ADDI x2, x0, 10
+        5'd2:  RD = 32'h002081B3; // ADD  x3, x1, x2
+        5'd3:  RD = 32'h40118233; // SUB  x4, x3, x1
+        5'd4:  RD = 32'h0020F2B3; // AND  x5, x1, x2
+        5'd5:  RD = 32'h0020E333; // OR   x6, x1, x2
+        5'd6:  RD = 32'h0020C3B3; // XOR  x7, x1, x2
+        5'd7:  RD = 32'h00109413; // SLLI x8, x1, 1
+        5'd8:  RD = 32'h0010D493; // SRLI x9, x1, 1
+        5'd9:  RD = 32'h00300513; // ADDI x10, x0, 3
+        5'd10: RD = 32'h00A505B3; // ADD  x11, x10, x10
+        5'd11: RD = 32'h00000000; // NOP
+
         default: RD = 32'h00000000;
     endcase
 end
