e92057643e73d0d28798cb27168c0be9cc09a962
Ahmed Elmallah <ah2005510@gmail.com>
2026-09-14 00:34:42 +0300

    Update Register_File.v

--- src/Register_File.v
+++ src/Register_File.v
@@ -6,12 +6,15 @@ module Register_File (
     input  [4:0]  A2,
     input  [4:0]  A3,
 
+    input  [4:0]  ReadRegAddr,
+
     input  [31:0] WD3,
 
     input         WE3,
 
     output [31:0] RD1,
-    output [31:0] RD2
+    output [31:0] RD2,
+    output [31:0] ReadRegData
 );
 
 reg [31:0] mem [0:31];
@@ -20,7 +23,12 @@ integer i;
 
 // Read ports
 assign RD1 = (A1 == 5'd0) ? 32'd0 : mem[A1];
+
 assign RD2 = (A2 == 5'd0) ? 32'd0 : mem[A2];
+
+// External read port
+assign ReadRegData =
+    (ReadRegAddr == 5'd0) ? 32'd0 : mem[ReadRegAddr];
 
 // Write port
 always @(posedge clk) begin
