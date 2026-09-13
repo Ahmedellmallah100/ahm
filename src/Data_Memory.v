c065b29272c120b26c6d7fdcc99ed621b0e73ee8
Ahmed Elmallah <ah2005510@gmail.com>
2026-09-14 00:27:53 +0300

    Refactor Data_Memory module structure and logic
    Refactor Data_Memory module to improve readability and functionality.

--- src/Data_Memory.v
+++ src/Data_Memory.v
@@ -1,33 +1,37 @@
-`timescale 1ns / 1ps
-`default_nettype none
-
 module Data_Memory (
-    input  wire        clk,
-    input  wire        WE,
-    input  wire        areset,
-    input  wire [31:0] A,
-    input  wire [31:0] WD,
-    output reg  [31:0] RD
+    input         clk,
+    input         WE,
+    input         areset,
+
+    input  [31:0] A,
+    input  [31:0] WD,
+
+    output [31:0] RD
 );
 
 reg [31:0] mem [0:7];
 
 integer i;
 
+// Read
+assign RD = mem[A[4:2]];
+
+// Write
 always @(posedge clk) begin
-    if (areset) begin
+
+    if (!areset) begin
+
         for (i = 0; i < 8; i = i + 1)
             mem[i] <= 32'd0;
-    end
-    else if (WE) begin
-        mem[A[4:2]] <= WD;
-    end
-end
 
-always @(*) begin
-    RD = mem[A[4:2]];
+    end
+
+    else if (WE) begin
+
+        mem[A[4:2]] <= WD;
+
+    end
+
 end
 
 endmodule
-
-`default_nettype wire
