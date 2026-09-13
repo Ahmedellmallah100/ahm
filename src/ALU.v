5ff5521b230b76c8e381040a151ce1f5e4639c10
Ahmed Elmallah <ah2005510@gmail.com>
2026-09-14 00:25:22 +0300

    Refactor ALU module with updated port declarations

--- src/ALU.v
+++ src/ALU.v
@@ -1,28 +1,35 @@
 module ALU (
-    ALuResult, SrcA, SrcB, ALUControl, zero_flag, sign_flag
+    input  [31:0] SrcA,
+    input  [31:0] SrcB,
+    input  [2:0]  ALUControl,
+    output reg [31:0] ALuResult,
+    output            zero_flag,
+    output            sign_flag
 );
 
-// Ports Declaration
-input  [31:0] SrcA, SrcB;
-input  [2:0]  ALUControl;
-output reg [31:0] ALuResult;
-output zero_flag, sign_flag;
-
-// Logic for ALU operations
 always @(*) begin
     case (ALUControl)
+
         3'b000: ALuResult = SrcA + SrcB;          // ADD
-        3'b001: ALuResult = SrcA << SrcB[4:0];    // SHL
+
+        3'b001: ALuResult = SrcA << SrcB[4:0];    // SLL
+
         3'b010: ALuResult = SrcA - SrcB;          // SUB
+
         3'b100: ALuResult = SrcA ^ SrcB;          // XOR
-        3'b101: ALuResult = SrcA >> SrcB[4:0];    // SHR
+
+        3'b101: ALuResult = SrcA >> SrcB[4:0];    // SRL
+
         3'b110: ALuResult = SrcA | SrcB;          // OR
+
         3'b111: ALuResult = SrcA & SrcB;          // AND
+
         default: ALuResult = 32'b0;
+
     endcase
- end
+end
 
 assign zero_flag = (ALuResult == 32'b0);
 assign sign_flag = ALuResult[31];
 
-endmodule  // ALU
+endmodule
