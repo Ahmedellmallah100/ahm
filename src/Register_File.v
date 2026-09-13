490659e49b2ecb6cd67f12e3c02964b6a0978d8c
Ahmed Elmallah <ah2005510@gmail.com>
2026-09-14 00:26:47 +0300

    Update Register_File.v

--- src/Register_File.v
+++ src/Register_File.v
@@ -1,37 +1,43 @@
 module Register_File (
-    A1, A2, A3, WD3, clk, areset, RD1, RD2, WE3
+    input         clk,
+    input         areset,
+
+    input  [4:0]  A1,
+    input  [4:0]  A2,
+    input  [4:0]  A3,
+
+    input  [31:0] WD3,
+
+    input         WE3,
+
+    output [31:0] RD1,
+    output [31:0] RD2
 );
 
-// Memory parameters
-parameter mem_width = 32;
-parameter mem_depth = 32;
-parameter addr_size = 5;
+reg [31:0] mem [0:31];
 
-// Port declaration
-input clk, areset;
-input [4:0]  A1, A2, A3;
-input [31:0] WD3;
-input WE3;
-output reg [31:0] RD1, RD2;
 integer i;
 
-// Memory array
-reg [mem_width-1:0] mem [mem_depth-1:0];
+// Read ports
+assign RD1 = (A1 == 5'd0) ? 32'd0 : mem[A1];
+assign RD2 = (A2 == 5'd0) ? 32'd0 : mem[A2];
 
-// Read operation (Asynchronous)
-always @(*) begin
-    RD1 = mem[A1];
-    RD2 = mem[A2];
-end
-
-// Write operation (Synchronous)
+// Write port
 always @(posedge clk) begin
+
     if (!areset) begin
-        for (i = 0; i < mem_depth; i = i + 1) begin
+
+        for (i = 0; i < 32; i = i + 1)
             mem[i] <= 32'd0;
-        end
-    end else if (WE3 && (A3 != 5'd0)) begin
-        mem[A3] <= WD3;
+
     end
+
+    else if (WE3 && (A3 != 5'd0)) begin
+
+        mem[A3] <= WD3;
+
+    end
+
 end
-endmodule  // Register_File
+
+endmodule
