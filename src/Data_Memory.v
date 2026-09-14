61bbc3c717d11d172bb74037fbfbee958fd8fd03
Ahmed Elmallah <ah2005510@gmail.com>
2026-09-14 22:12:37 +0300

    Update Data_Memory.v

--- src/Data_Memory.v
+++ src/Data_Memory.v
@@ -1,32 +1,48 @@
 module Data_Memory (
-    input         clk,
-    input         areset,
+    input        clk,
+    input        areset,
 
-    input  [7:0]  A,
-    input  [7:0]  WD,
-    input         WE,
+    input  [7:0] A,
+    input  [7:0] WD,
+    input        WE,
 
-    output [7:0]  RD
+    output [7:0] RD
 );
 
 reg [7:0] mem [0:15];
 
 integer i;
 
-/* Asynchronous read */
+
+/* =========================
+   Asynchronous Read
+   ========================= */
+
 assign RD =
-    (A < 8'd16) ? mem[A] :
+    (A < 8'd16) ?
+    mem[A[3:0]] :
     8'd0;
 
-/* Synchronous write */
+
+/* =========================
+   Synchronous Write
+   ========================= */
+
 always @(posedge clk) begin
+
     if (!areset) begin
+
         for (i = 0; i < 16; i = i + 1)
             mem[i] <= 8'd0;
+
     end
+
     else if (WE && (A < 8'd16)) begin
-        mem[A] <= WD;
+
+        mem[A[3:0]] <= WD;
+
     end
+
 end
 
 endmodule
