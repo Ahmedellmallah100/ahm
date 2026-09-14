a089e412548d53e260adee87e71cc2582ea83e60
Ahmed Elmallah <ah2005510@gmail.com>
2026-09-14 03:06:05 +0300

    Refactor Register_File module for clarity

--- src/Register_File.v
+++ src/Register_File.v
@@ -9,7 +9,6 @@ module Register_File (
     input  [4:0]  ReadRegAddr,
 
     input  [31:0] WD3,
-
     input         WE3,
 
     output [31:0] RD1,
@@ -21,29 +20,24 @@ reg [31:0] mem [0:31];
 
 integer i;
 
-// Read ports
-assign RD1 = (A1 == 5'd0) ? 32'd0 : mem[A1];
+assign RD1 =
+    (A1 == 5'd0) ? 32'd0 : mem[A1];
 
-assign RD2 = (A2 == 5'd0) ? 32'd0 : mem[A2];
+assign RD2 =
+    (A2 == 5'd0) ? 32'd0 : mem[A2];
 
-// External read port
 assign ReadRegData =
     (ReadRegAddr == 5'd0) ? 32'd0 : mem[ReadRegAddr];
 
-// Write port
 always @(posedge clk) begin
 
     if (!areset) begin
-
         for (i = 0; i < 32; i = i + 1)
             mem[i] <= 32'd0;
-
     end
 
     else if (WE3 && (A3 != 5'd0)) begin
-
         mem[A3] <= WD3;
-
     end
 
 end
