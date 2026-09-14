971fdce6d1f93e2e98d2433ba807f0b9eb80c76f
Ahmed Elmallah <ah2005510@gmail.com>
2026-09-14 03:06:14 +0300

    Update Data_Memory.v

--- src/Data_Memory.v
+++ src/Data_Memory.v
@@ -7,19 +7,6 @@ module Data_Memory (
     output [31:0] RD
 );
 
-reg [31:0] mem [0:7];
-integer i;
-
-assign RD = mem[A[4:2]];
-
-always @(posedge clk) begin
-    if (!areset) begin
-        for (i = 0; i < 8; i = i + 1)
-            mem[i] <= 32'd0;
-    end
-    else if (WE) begin
-        mem[A[4:2]] <= WD;
-    end
-end
+assign RD = 32'b0;
 
 endmodule
