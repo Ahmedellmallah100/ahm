f68db65dad46ff6d9d17a779e7a45d3400b12ca1
Ahmed Elmallah <ah2005510@gmail.com>
2026-09-14 03:16:13 +0300

    Refactor Register_File for 8-bit data width

--- src/Register_File.v
+++ src/Register_File.v
@@ -8,49 +8,41 @@ module Register_File (
 
     input  [4:0]  ReadRegAddr,
 
-    input  [31:0] WD3,
+    input  [7:0]  WD3,
     input         WE3,
 
-    output [31:0] RD1,
-    output [31:0] RD2,
-    output [31:0] ReadRegData
+    output [7:0] RD1,
+    output [7:0] RD2,
+    output [7:0] ReadRegData
 );
 
-// 16 registers: x0 - x15
-reg [31:0] mem [0:15];
+reg [7:0] mem [0:15];
 
 integer i;
 
-// Read port 1
 assign RD1 =
-    (A1 == 5'd0) ? 32'd0 :
+    (A1 == 5'd0) ? 8'd0 :
     (A1 < 5'd16) ? mem[A1] :
-    32'd0;
+    8'd0;
 
-// Read port 2
 assign RD2 =
-    (A2 == 5'd0) ? 32'd0 :
+    (A2 == 5'd0) ? 8'd0 :
     (A2 < 5'd16) ? mem[A2] :
-    32'd0;
+    8'd0;
 
-// External register read
 assign ReadRegData =
-    (ReadRegAddr == 5'd0) ? 32'd0 :
+    (ReadRegAddr == 5'd0) ? 8'd0 :
     (ReadRegAddr < 5'd16) ? mem[ReadRegAddr] :
-    32'd0;
+    8'd0;
 
-// Write port
 always @(posedge clk) begin
-
     if (!areset) begin
         for (i = 0; i < 16; i = i + 1)
-            mem[i] <= 32'd0;
+            mem[i] <= 8'd0;
     end
-
     else if (WE3 && (A3 != 5'd0) && (A3 < 5'd16)) begin
         mem[A3] <= WD3;
     end
-
 end
 
 endmodule
