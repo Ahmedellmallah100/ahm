e938db831ac7a1b017047789d8b2201e5eb61f58
Ahmed Elmallah <ah2005510@gmail.com>
2026-09-14 03:20:15 +0300

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,18 +1,38 @@
+module tt_um_example (
+    input wire [7:0] ui_in,
+    output wire [7:0] uo_out,
+    input wire [7:0] uio_in,
+    output wire [7:0] uio_out,
+    output wire [7:0] uio_oe,
+    input wire ena,
+    input wire clk,
+    input wire rst_n
+);
+
+wire [4:0] instr_addr;
+assign instr_addr = ui_in[4:0];
+
+wire execute;
+assign execute = ui_in[5];
+
+wire [1:0] output_select;
+assign output_select = ui_in[7:6];
+
+wire [4:0] read_reg_addr;
+assign read_reg_addr = uio_in[4:0];
+
 wire [31:0] Instruction;
-wire [7:0]  ALUResult;
-wire [7:0]  ReadRegData;
-wire [7:0]  MemoryData;
-wire [7:0]  Result;
+wire [7:0] ALUResult;
+wire [7:0] ReadRegData;
+wire [7:0] MemoryData;
+wire [7:0] Result;
 
 riscv_core core_inst (
     .clk(clk),
     .areset(rst_n),
-
     .InstrAddr(instr_addr),
     .Execute(execute),
-
     .ReadRegAddr(read_reg_addr),
-
     .Instruction(Instruction),
     .ALUResult(ALUResult),
     .ReadRegData(ReadRegData),
@@ -33,3 +53,11 @@ always @(*) begin
 end
 
 assign uo_out = selected_data;
+
+assign uio_out = 8'b0;
+assign uio_oe  = 8'b0;
+
+wire _unused;
+assign _unused = &{ena, uio_in[7:5]};
+
+endmodule
