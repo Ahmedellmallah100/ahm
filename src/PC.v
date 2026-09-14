10385c224a5a322a05adb48c6c4e8d803616f3fc
Ahmed Elmallah <ah2005510@gmail.com>
2026-09-14 22:29:41 +0300

    Update PC.v

--- src/PC.v
+++ src/PC.v
@@ -1,19 +1,16 @@
 module PC (
     input         clk,
     input         areset,
-    input  [7:0]  PC_Next,
+    input  [7:0]   PC_Next,
     output reg [7:0] PC,
     input         Load
 );
 
 always @(posedge clk or negedge areset) begin
-
     if (!areset)
         PC <= 8'd0;
-
     else if (Load)
         PC <= PC_Next;
-
 end
 
 endmodule
