f60bd6679b8fb93294f4c11005af426ccbaaf68a
Ahmed Elmallah <ah2005510@gmail.com>
2026-09-14 03:10:49 +0300

    Update Register_File.v

--- src/Register_File.v
+++ src/Register_File.v
@@ -16,27 +16,38 @@ module Register_File (
     output [31:0] ReadRegData
 );
 
-reg [31:0] mem [0:31];
+// 16 registers: x0 - x15
+reg [31:0] mem [0:15];
 
 integer i;
 
+// Read port 1
 assign RD1 =
-    (A1 == 5'd0) ? 32'd0 : mem[A1];
+    (A1 == 5'd0) ? 32'd0 :
+    (A1 < 5'd16) ? mem[A1] :
+    32'd0;
 
+// Read port 2
 assign RD2 =
-    (A2 == 5'd0) ? 32'd0 : mem[A2];
+    (A2 == 5'd0) ? 32'd0 :
+    (A2 < 5'd16) ? mem[A2] :
+    32'd0;
 
+// External register read
 assign ReadRegData =
-    (ReadRegAddr == 5'd0) ? 32'd0 : mem[ReadRegAddr];
+    (ReadRegAddr == 5'd0) ? 32'd0 :
+    (ReadRegAddr < 5'd16) ? mem[ReadRegAddr] :
+    32'd0;
 
+// Write port
 always @(posedge clk) begin
 
     if (!areset) begin
-        for (i = 0; i < 32; i = i + 1)
+        for (i = 0; i < 16; i = i + 1)
             mem[i] <= 32'd0;
     end
 
-    else if (WE3 && (A3 != 5'd0)) begin
+    else if (WE3 && (A3 != 5'd0) && (A3 < 5'd16)) begin
         mem[A3] <= WD3;
     end
 
