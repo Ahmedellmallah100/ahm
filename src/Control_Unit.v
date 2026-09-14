0b992c7637ff3f607b0f760b98b5a7fb986ee40c
Ahmed Elmallah <ah2005510@gmail.com>
2026-09-14 22:01:22 +0300

    Update Control_Unit.v

--- src/Control_Unit.v
+++ src/Control_Unit.v
@@ -7,9 +7,7 @@ module Control_Unit (
     output reg       ALUSrc,
     output reg       RegWrite,
     output reg       MemWrite,
-    output reg       PCSrc,
-    output reg       ResultSrc,
-    output reg [1:0] ImmSrc,
+    output reg       MemToReg,
     output reg       Branch
 );
 
@@ -19,9 +17,7 @@ always @(*) begin
     ALUSrc     = 1'b0;
     RegWrite   = 1'b0;
     MemWrite   = 1'b0;
-    PCSrc      = 1'b0;
-    ResultSrc  = 1'b0;
-    ImmSrc     = 2'b00;
+    MemToReg   = 1'b0;
     Branch     = 1'b0;
 
     case (opcode)
@@ -30,6 +26,7 @@ always @(*) begin
            I-Type
            ADDI / SLLI / SRLI
            ========================= */
+
         7'b0010011: begin
 
             ALUSrc   = 1'b1;
@@ -38,13 +35,13 @@ always @(*) begin
             case (funct3)
 
                 3'b000:
-                    ALUControl = 3'b000;   // ADDI
+                    ALUControl = 3'b000; // ADDI
 
                 3'b001:
-                    ALUControl = 3'b001;   // SLLI
+                    ALUControl = 3'b001; // SLLI
 
                 3'b101:
-                    ALUControl = 3'b101;   // SRLI
+                    ALUControl = 3'b101; // SRLI
 
                 default:
                     ALUControl = 3'b000;
@@ -56,6 +53,7 @@ always @(*) begin
         /* =========================
            R-Type
            ========================= */
+
         7'b0110011: begin
 
             ALUSrc   = 1'b0;
@@ -64,20 +62,22 @@ always @(*) begin
             case (funct3)
 
                 3'b000: begin
+
                     if (funct7)
-                        ALUControl = 3'b010;   // SUB
+                        ALUControl = 3'b010; // SUB
                     else
-                        ALUControl = 3'b000;   // ADD
+                        ALUControl = 3'b000; // ADD
+
                 end
 
                 3'b100:
-                    ALUControl = 3'b100;       // XOR
+                    ALUControl = 3'b100; // XOR
 
                 3'b110:
-                    ALUControl = 3'b110;       // OR
+                    ALUControl = 3'b110; // OR
 
                 3'b111:
-                    ALUControl = 3'b111;       // AND
+                    ALUControl = 3'b111; // AND
 
                 default:
                     ALUControl = 3'b000;
@@ -87,24 +87,50 @@ always @(*) begin
 
 
         /* =========================
-           B-Type
            BEQ
            ========================= */
+
         7'b1100011: begin
 
-            Branch   = 1'b1;
-            ALUSrc   = 1'b0;
+            Branch = 1'b1;
+
+            ALUSrc     = 1'b0;
+            RegWrite   = 1'b0;
+            MemWrite   = 1'b0;
+            MemToReg   = 1'b0;
+
+            ALUControl = 3'b010; // SUB
+
+        end
+
+
+        /* =========================
+           LW
+           ========================= */
+
+        7'b0000011: begin
+
+            ALUSrc   = 1'b1;
+            RegWrite = 1'b1;
+            MemToReg = 1'b1;
+
+            ALUControl = 3'b000; // address calculation
+
+        end
+
+
+        /* =========================
+           SW
+           ========================= */
+
+        7'b0100011: begin
+
+            ALUSrc   = 1'b1;
             RegWrite = 1'b0;
+            MemWrite = 1'b1;
 
-            case (funct3)
+            ALUControl = 3'b000; // address calculation
 
-                3'b000:
-                    ALUControl = 3'b010;   // SUB for BEQ
-
-                default:
-                    ALUControl = 3'b010;
-
-            endcase
         end
 
 
@@ -113,11 +139,14 @@ always @(*) begin
             ALUControl = 3'b000;
             ALUSrc     = 1'b0;
             RegWrite   = 1'b0;
+            MemWrite   = 1'b0;
+            MemToReg   = 1'b0;
             Branch     = 1'b0;
 
         end
 
     endcase
+
 end
 
 endmodule
